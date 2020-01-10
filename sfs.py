733d75b8e315d5749e071cccd02ed8620750c809
Daniel <daniel.jordan1@mssm.edu>
2020-01-09 19:09:05 -0500

    Changed SFSs from np.ndarray to pd.SparseArray

--- sfs.py
+++ sfs.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 import itertools
+from functools import reduce
 from typing import TextIO, Union
 
 import attr
@@ -50,7 +51,11 @@ class SFSUnroller:
         return sfs
 
 
-def load_exac_sfs(exac_filename: str, cpg_filename: str, sample_size: int = 68858) -> pd.Series:
+def load_exac_sfs(exac_filename: str, cpg_filename: str, sample_size: int = 68858) -> pd.DataFrame:
+    """
+    Read tab-separated files containing information on ExAC variants, filter by coverage and CpG status,
+    and return a DataFrame where columns are SFSs (indexed by DAC in NFE), and column keys are (gene, func).
+    """
     nt_dtype = pd.api.types.CategoricalDtype(["A", "C", "T", "G"])
 
     cpg_df = pd.read_csv(cpg_filename, delim_whitespace=True, header=None,
@@ -67,18 +72,21 @@ def load_exac_sfs(exac_filename: str, cpg_filename: str, sample_size: int = 6885
     exac_df = exac_df.rename({"gene_canonical" : "gene"}, axis='columns')
     exac_consequences = exac_df.type_canonical.str.get_dummies("&").join(exac_df.pphpred.str.get_dummies()).astype(bool)
 
-    unroll_sfs = SFSUnroller(sample_size)
-    func_sfsa = []
+    func_sfsa = {}
     for output_func, input_types in func_types_exac.items():
-        sfs_sparse = exac_df[exac_consequences.eval(" or ".join(input_types))].groupby("gene").DAC.value_counts()
-        sfs_unrolled = sfs_sparse.groupby(level="gene").apply(unroll_sfs)
-        sfs_df = pd.DataFrame.from_dict({'func' : output_func, 'alleles' : sfs_unrolled}).set_index("func", append=True)
-        func_sfsa.append(sfs_df)
+        sfs_df = exac_df[exac_consequences.eval(" or ".join(input_types))].groupby("gene").DAC.value_counts()
+        sfs_df = sfs_df.unstack(level="gene", fill_value=0)
+        sfs_df = sfs_df.reindex(pd.RangeIndex(1,sample_size), fill_value=0)
+        sfs_df = sfs_df.transform(compress_sfs)
+        func_sfsa[output_func] = sfs_df
 
-    return pd.concat(func_sfsa).sort_index().squeeze()
+    return pd.concat(func_sfsa, axis=1, names=["func", "gene"]).swaplevel(axis="columns").sort_index(axis="columns")
 
 
 def load_exac_log_lengths(func_types_filename: str, pph_filename: str, mu: float = 1e-8) -> pd.Series:
+    """
+    Reads CSVs containing target sizes and returns a Series containing gene lengths, indexed by (gene, func)
+    """
     func_types_table = pd.read_csv(func_types_filename, index_col=None).rename({"type" : "func"}, axis="columns")
     pph_table = pd.read_csv(pph_filename, index_col=None).rename({"prediction" : "func"}, axis="columns")
     combined_table = func_types_table.append(pph_table)
@@ -90,45 +98,77 @@ def load_exac_log_lengths(func_types_filename: str, pph_filename: str, mu: float
     return np.log10(rates_df.stack().squeeze() / mu)
 
 
-def load_simulated_sfs(filename: str) -> pd.Series:
-    return pd.read_csv(filename, sep="\t", header=0, names=["DAC", "alleles"]).set_index("DAC").drop(0)
+def load_simulated_sfs(filename: str, sample_size: int = 68858, compress=True) -> pd.Series:
+    """
+    Reads the sparse tab-delimited SFS format output by SimDose and returns a single simulated SFS as a Series,
+    indexed by DAC.
+    """
+    data = pd.read_csv(filename, sep="\t", header=0, names=["DAC", "alleles"], index_col="DAC", squeeze=True) # load data
+    data = data.reindex(pd.RangeIndex(1, sample_size), fill_value=0) # drop the 0 DAC row and fill in zero counts
+    if compress:
+        data = compress_sfs(data)
+    return data
 
 
-def load_simulated_sfs_genes(template: str, trials: int = 1000, sample_size: int = 68858) -> pd.Series:
+def compress_sfs(data: pd.Series) -> pd.Series:
+    if pd.api.types.is_integer_dtype(data):
+        # find the smallest dtype to represent the data
+        # we only want to do this for integers
+        for dtype in np.uint8, np.uint16, np.uint32, np.uint64:
+            if data.lt(np.iinfo(dtype).max).all():
+                break
+    else:
+        dtype = data.dtype.type
+    data = data.astype(pd.SparseDtype(dtype, fill_value=dtype(0)))  # compress representation in memory
+    return data
+
+
+def load_simulated_sfs_genes(template: str, trials: int = 1000, sample_size: int = 68858) -> pd.DataFrame:
+    """
+    Iterates over all combinations of h, s, logL, and seed (up to the specified value) and returns a DataFrame
+    whose columns are SFSs. Columns are indexed by h, s, logL, and seed; rows are indexed by DAC.
+    """
     sim_dict = {}
     with tqdm.tqdm(total=17*31*trials) as pbar:
         for h, s in itertools.chain([(0.5, "NEUTRAL")], itertools.product([0.0, 0.1, 0.3, 0.5], ["-1.0", "-2.0", "-3.0", "-4.0"])):
             for logL in np.arange(2.0, 5.1, 0.1).round(1):
                 for seed in range(1, trials+1):
                     filename = template.format(h=h, s=s, logL=logL, seed=seed)
-                    sim_dict[h, s, logL, seed] = load_simulated_sfs(filename)
+                    sim_dict[h, s, logL, seed] = load_simulated_sfs(filename, sample_size)
                     pbar.update(1)
-    return pd.concat(sim_dict, names=["h", "s", "logL", "seed"]).groupby(level=["h","s","logL","seed"]).apply(SFSUnroller(sample_size)).squeeze()
+    return pd.concat(sim_dict, names=["h", "s", "logL", "seed"], axis=1)
 
 
-def load_simulated_sfs_reference(template: str, reference_L: float = 1e9, sample_size: int = 68858) -> pd.Series:
+def load_simulated_sfs_reference(template: str, reference_L: float = 1e9, sample_size: int = 68858) -> pd.DataFrame:
+    """
+    Iterates over all combinations of h and s and returns a DataFrame whose columns are SFSs, scaled to L = 1.
+    Columns are indexed by h and s; rows are indexed by DAC.
+    """
     sim_dict = {}
     for h, s in itertools.chain([(0.5, "NEUTRAL")], itertools.product([0.0, 0.1, 0.3, 0.5], ["-1.0", "-2.0", "-3.0", "-4.0"])):
         filename = template.format(h=h, s=s)
-        sim_dict[h,s] = load_simulated_sfs(filename)
-    return pd.concat(sim_dict, names=["h", "s"]).groupby(level=["h","s"]).apply(SFSUnroller(sample_size)).squeeze() / reference_L
+        sim_dict[h,s] = load_simulated_sfs(filename, sample_size) / reference_L
+    return pd.concat(sim_dict, names=["h", "s"], axis=1)
 
 
-def calculate_prf_loglikelihood(reference_sfs_series: pd.Series, observed_sfs_series: pd.Series, observed_logL: pd.Series) -> pd.DataFrame:
+def calculate_prf_loglikelihood(reference_sfs_df: pd.DataFrame, observed_sfs_df: pd.DataFrame, observed_logL: pd.Series) -> pd.DataFrame:
+    """
+    Takes a reference_sfs DataFrame keyed by h and s, an observed_sfs DataFrame keyed by any set of keys,
+    and an observed_logL Series keyed by the same key as observed_sfs.
+    Returns a DataFrame whose rows are the input keys and columns are (h,s).
+    """
     def single_key_loglikelihood(key):
-        scaled_reference_sfs = reference_sfs_series * 10**(observed_logL.get(key, np.nan))
-        observed_sfs = observed_sfs_series.loc[key]
-        def single_reference_loglikelihood(reference_sfs):
-            poisson_logpmfs = observed_sfs * np.log(reference_sfs) - reference_sfs - special.gammaln(observed_sfs + 1)
-            return np.nansum(poisson_logpmfs)
-        return scaled_reference_sfs.apply(single_reference_loglikelihood)
-    keys = observed_sfs_series.index.to_series()
+        lamda = reference_sfs_df * 10 ** (observed_logL.get(key, np.nan))
+        k = observed_sfs_df[key]
+        log_k_factorial = special.gammaln(observed_sfs + 1)
+        return pd.eval("k * log(lamda.T) - lambda - log_k_factorial").agg(np.nansum)
+    keys = observed_sfs_df.keys().to_series()
     return keys.apply(single_key_loglikelihood)
 
 
-def compute_summary_stats(unrolled_sfs: pd.Series, sample_size: int = 68858) -> pd.DataFrame:
-    xbar = unrolled_sfs.apply(lambda x: np.arange(1, sample_size + 1) @ x / sample_size)
-    logstat = unrolled_sfs.apply(lambda x: np.log1p(x).sum())
+def compute_summary_stats(sfs_df: pd.DataFrame, sample_size: int = 68858) -> pd.DataFrame:
+    xbar = sfs_df.agg(lambda x: x @ x.index / sample_size)
+    logstat = sfs_df.transform(np.log1p).sum()
     return pd.DataFrame.from_dict({'xbar' : xbar, 'logstat' : logstat})
 
 
